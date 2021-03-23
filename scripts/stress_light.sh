4eaf7d7531e29019ea5537e944de3fd2d87aefff
ubuntu <yuanhuaiw@nvidia.com>
2021-03-23 06:12:28 +0000

    Initial commit

---
+++ scripts/stress_light.sh
@@ -0,0 +1,12 @@
+#/bin/sh
+i=0
+URL=<TRITON_EXTERNAL_IP>
+BATCH_SIZE=32
+while :
+do
+        echo "Loop $i, sending $BATCH_SIZE requests to Triton Inference Server"
+        /workspace/install/bin/image_client -m inception_graphdef -c 1 -s INCEPTION /workspace/images/mug.jpg -u $URL:8000 -b $BATCH_SIZE >> /dev/null &
+        sleep 0.5
+        i=$(($i+1))
+done
+
